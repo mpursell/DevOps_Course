419cbcfe9075835f9564b9f840456eeca1c3ef98
Mike <michael.pursell@gmail.com>
2021-10-28 21:04:36 +0100

    comment cleanup

--- DevOps_Module1/Exercises/FizzBuzzBangBongFezz.py
+++ DevOps_Module1/Exercises/FizzBuzzBangBongFezz.py
@@ -4,6 +4,8 @@ def checkFactors(num):
 
     # function to return the number of factors of a number and the factors themselves
     # returns a tuple where [0] is the number of factors and [1] is the list of factors
+    # e.g. (3, [1,3,5])
+    # if only two factors return we know that the number can only divisible by itself and 1
 
     # get the range of numbers we need (plus 1 since Python doesn't count
     # the last number in the range)
@@ -17,30 +19,16 @@ def checkFactors(num):
 
     numberOfFactors  = len(factorList)
 
-    #print ('There are ' + numberOfFactors + ' factors of ' + str(num))
-    #for result in factorList:
-    #     print(result)
-
     return (numberOfFactors, factorList)
 
-#checkFactors(13)[1]
+
 
 def fizzBuzzBangBongFezz(startNumber, endNumber):
 
-    # set the range and instantiate a dict with number=number key / value
+    # set the range and instantiate a dict with number:'' key / value
     # for the given range
     numbersRange = range(startNumber, endNumber)
     numbersDict = {}
-
-    # strings for printing
-
-    str_three_and_five = 'FizzBuzz'
-    str_three = 'Fizz'
-    str_five = 'Buzz'
-    str_seven = 'Bang'
-    str_eleven = 'Bong'
-    str_thirteen = 'Fezz'    
-
 
     # setup dict with {1:1, 2:2, 3:3...etc}
     for num in numbersRange:
@@ -48,7 +36,6 @@ def fizzBuzzBangBongFezz(startNumber, endNumber):
 
     # update the dictionary with FizzBuzz / Bang / Bong ruleset
     for num in numbersRange:
-
 
         if (3 in checkFactors(num)[1]) and (5 in checkFactors(num)[1]):
             numbersDict.update({num:'FizzBuzz'})
@@ -82,10 +69,14 @@ def fizzBuzzBangBongFezz(startNumber, endNumber):
             strDictValue = numbersDict[num]
             numbersDict.update({num:'Fezz'})
 
+        # if 13 is a factor, but there are more than 2 factors, add 'Fezz' to the start
+        ######  NEEDS TO DO STRING INSERT OF FEZZ AT FIRST B #########
         elif (13 in checkFactors(num)[1]):
             strDictValue = str(numbersDict[num])
             numbersDict.update({num:'Fezz' + strDictValue})
 
+
+        # if a number has 17 as a factor, reverse the Fizz, Buzz, Bang
         elif(17 in checkFactors(num)[1]):
 
             # get the value of the key {num} as a string
@@ -101,48 +92,6 @@ def fizzBuzzBangBongFezz(startNumber, endNumber):
             strDictValue = "".join(strDictValueList)
 
             numbersDict.update({num:strDictValue})
-
-
-
-
-
-
-
-
-
-
-
-        #if checkFactor(3, num) == 0  and checkFactor(7, num) == 0:
-            
-        #elif checkFactor(5, num) == 0 and checkFactor(7, num) == 0:
-         #   numbersDict.update({num:'BuzzBang'})
-        #elif checkFactor(3, num) == 0 and checkFactor(5, num) == 0:
-        #    numbersDict.update({num:'FizzBuzz '})
-    
-        # check if number is also a multiple of 11, if it isn't print as per this
-        # elif statement, otherwise we want to allow number to fall through to 
-        # "Bong" condition
-        #elif checkFactor(3, num) == 0 and checkFactor(11, num) != 0:
-            #numbersDict.update({num:'Fizz'})
-        #elif checkFactor(5, num) == 0 and checkFactor(11, num) != 0:
-          #  numbersDict.update({num:'Buzz'})
-        #elif checkFactor(7, num) == 0 and checkFactor(11, num) != 0:
-           # numbersDict.update({num:'Bang'})
-        #elif checkFactor(11, num) == 0:
-           # numbersDict.update({num:'Bong'})
-        
-    
-    
-    # iterate over the dictionary with the previous ruleset and check:
-    #  1. if the key is a multiple of 13, string add / replace to the value with "Fezz"
-    #   in front of the first 'B'
-    #  2. if the key is a multiple of 17, string split on capital letters and reverse
-    #   word order
-    #for numbersDictKey, numbersDictValue in numbersDict.items():
-        
-     #   if checkFactor(13, numbersDictKey) == 0:
-      #      numbersDict.update({numbersDictKey:'Fezz'})
-        
         
     # print out the key -> value pairs
     for numbersDictKey, numbersDictValue in numbersDict.items():
